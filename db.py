41e1b700290b62ac10b5023b3b3166ef3b17b32d
Mike Parker <mgp@khanacademy.org>
2014-10-24 15:31:37 -0700

    Start of db

---
+++ db.py
@@ -0,0 +1,85 @@
+from datetime import datetime
+import functools
+import sqlalchemy as sa
+import sqlalchemy.engine as sa_engine
+import sqlalchemy.ext.declarative as sa_ext_declarative
+import sqlalchemy.orm as sa_orm
+import sys
+
+
+def close_session(f):
+  """A decorator that closes the session before returning a result.
+  """
+
+  @functools.wraps(f)
+  def decorated_function(*pargs, **kwargs):
+    result = f(*pargs, **kwargs)
+    session.close()
+    return result
+  return decorated_function
+
+
+class DbException(Exception):
+  """Exception class raised by the database.
+  """
+
+  def __init__(self, reason):
+    Exception.__init__(self)
+    self.reason = reason
+
+  def __str__(self):
+    return str(self.reason)
+
+  @staticmethod
+  def _chain():
+    exception_type, exception_value, traceback = sys.exc_info()
+    raise DbException(exception_value), None, traceback
+
+
+def _utcnow(now):
+  """Returns the given time if not None, or datetime.utcnow() otherwise.
+  """
+  if now is None:
+    return datetime.utcnow()
+  return now
+
+
+def optional_one(query):
+  """Like calling one() on query, but returns None instead of raising
+  NoResultFound.
+  """
+  results = query.limit(1).all()
+  if results:
+    return results[0] 
+  return None
+
+
+_Base = sa_ext_declarative.declarative_base()
+_engine = None
+session = None
+def create_session(database, database_uri):
+  global _engine
+  global session
+
+  if database == 'sqlite':
+    # http://docs.sqlalchemy.org/en/rel_0_9/dialects/sqlite.html#foreign-key-support
+    @sa.event.listens_for(sa_engine.Engine, "connect")
+    def set_sqlite_pragma(dbapi_connection, connection_record):
+      cursor = dbapi_connection.cursor()
+      cursor.execute("PRAGMA foreign_keys=ON")
+      cursor.close()
+    _engine = sa.create_engine(database_uri, convert_unicode=True, echo=False)
+
+    # Use scoped_session with Flask: http://flask.pocoo.org/docs/patterns/sqlalchemy/
+    session = sa_orm.scoped_session(sa_orm.sessionmaker(
+        autocommit=False, autoflush=False, bind=_engine))
+
+
+def create_all_tables():
+  """Creates all tables and indexes in the database."""
+  _Base.metadata.create_all(_engine)
+
+def drop_all_tables():
+  """Drops all tables and indexes in the database."""
+  _Base.metadata.drop_all(_engine)
+
