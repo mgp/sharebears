32eec6dbabe978e60b6e5d67ccb880ef99dd36bf
Mike Parker <mgp@khanacademy.org>
2014-10-26 20:16:26 -0700

    Start of URL decoder and client for GitHub

---
+++ sharebears/github_client.py
@@ -0,0 +1,50 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+class GitHubClientException(Exception):
+  """An exception raised by a GitHubClient."""
+
+  def __init__(self, reason):
+    Exception.__init__(self)
+    self.reason = reason
+
+  def __str__(self):
+    return str(self.reason)
+
+
+class GitHubClient:
+  """A client for communicating with the GitHub API."""
+
+  _ROOT_ENDPOINT = "https://api.github.com"
+
+  # TODO: Use a token for auth?
+  def __init__(self, username, password):
+    self.username = username
+    self.password = password
+
+  def _get_auth(self):
+    return HTTPBasicAuth(self.username, self.password)
+
+  def _json_from_response_for_path(self, path):
+    url = "%s/%s" % (_GitHubClient._ROOT_ENDPOINT, path)
+    response = requests.get(url, auth=self._get_auth())
+    if response.status_code == 200:
+      return response.json()
+    else:
+      raise GitHubClientException("Status code was not 200 but was %s" % response.status_code)
+  
+  def get_repository(self, owner, repo):
+    """Returns the JSON for the given repository by the given owner."""
+
+    # See https://developer.github.com/v3/repos/#get
+    path = "/repos/%s/%s" % (owner, repo)
+    return self._json_from_response_for_path(path)
+
+  def get_commit(self, owner, repo, sha):
+    """Returns the JSON for the commit with the given SHA."""
+
+    # See https://developer.github.com/v3/git/commits/#get-a-commit
+    path = "/repos/%s/%s/git/commits/%s" % (owner, repo, sha)
+    return self._json_from_response_for_path(path)
+
---
+++ sharebears/url_decoder_github.py
@@ -0,0 +1,133 @@
+from url_decoder import UrlDecoder, UrlDecoderException, filter_json
+
+import re
+
+
+class _GitHubUrlDecoder(UrlDecoder):
+  def is_decodeable_url(self, url, parsed_url):
+    if not parsed_url.netloc.startswith("github."):
+      return False
+    return True
+
+
+class GitHubRepositoryUrlDecoder(_GitHubUrlDecoder):
+  """Renders a GitHub repository."""
+
+  _PATH_REGEX = re.compile("^(?P<owner>\w+)/(?P<repo>\w+)$")
+
+  def __init__(self, github_client):
+    self.github_client = github_client
+
+  def name(self):
+    return "github-repository"
+
+  def _match_parsed_url(self, parsed_url):
+    return GitHubRepositoryUrlDecoder._PATH_REGEX.match(parsed_url.path)
+
+  def is_decodeable_url(self, url, parsed_url):
+    if not GitHubUrlDecoder.is_decodeable_url(self, url, parsed_url):
+      return False
+    elif not self._match_parsed_url(parsed_url):
+      return False
+    return True
+
+  def _filter_json(self, json):
+    """Filters the JSON from https://developer.github.com/v3/repos/#get"""
+
+    # Filter the repository owner.
+    owner_json = json["owner"]
+    filtered_owner_json = filter_json(owner_json, "login", "avatar_url", "html_url")
+
+    # Filter the repository.
+    filtered_json = filter_json(json,
+        "name",
+        "description",
+        "html_url",
+        "forks_count",
+        "stargazers_count",
+        "watchers_count",
+        "created_at",
+        "updated_at")
+
+    filtered_json["owner"] = filtered_owner_json
+    return filtered_json
+
+  def decode_url(self, url, parsed_url):
+    match = self._match_parsed_url(parsed_url)
+    if not match:
+      raise UrlDecoderException("URL is not decodeable: %s" % parsed_url)
+    owner = match.group("owner")
+    repo = match.group("repo")
+    json = self.github_client.get_repository(owner, repo)
+    return self._filter_json(json)
+
+  def render_decoded_url(self, decoded_url):
+    # TODO
+    pass
+
+
+class GitHubCommitUrlDecoder(_GitHubUrlDecoder):
+  """Renders a commit belonging to a GitHub repository."""
+
+  _PATH_REGEX = re.compile("^(?P<owner>\w+)/(?P<repo>\w+)/commit/(?P<sha>\w+)$")
+
+  def __init__(self, github_client):
+    self.github_client = github_client
+
+  def name(self):
+    return "github-commit"
+
+  def _match_parsed_url(self, parsed_url):
+    return GitHubCommitUrlDecoder._PATH_REGEX.match(parsed_url.path)
+
+  def is_decodeable_url(self, url, parsed_url):
+    if not GitHubUrlDecoder.is_decodeable_url(self, url, parsed_url):
+      return False
+    elif not self._match_parsed_url(parsed_url)
+      return False
+    return True
+
+  def _filter_json(self, json):
+    """Filters the JSON from https://developer.github.com/v3/git/commits/#get-a-commit"""
+
+    return filter_json(json,
+        "sha",
+        "url",
+        "author",
+        "committer",
+        "message")
+
+  def decode_url(self, url, parsed_url):
+    match = self._match_parsed_url(parsed_url)
+    if not match:
+      raise UrlDecoderException("URL is not decodeable: %s" % parsed_url)
+    owner = match.group("owner")
+    repo = match.group("repo")
+    sha = match.group("sha")
+    json = self.github_client.get_commit(owner, repo, sha)
+    return self._filter_json(json)
+
+  def render_decoded_url(self, decoded_url):
+    # TODO
+    pass
+
+
+class GitHubGistUrlDecoder(UrlDecoder):
+  """Embeds a Gist."""
+
+  def name(self):
+    return "github-gist"
+
+  def is_decodeable_url(self, url, parsed_url):
+    if not parsed_url.netloc.startswith("gist.github."):
+      return False
+    return True
+
+  def decode_url(self, url, parsed_url):
+    # Use an embedded Gist.
+    return { "url": url }
+
+  def render_decoded_url(self, decoded_url):
+    # TODO
+    pass
+
